580d19f444fa72f4340a6c0594c5de8de89864c7
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-21 05:16:38 +0000

    up

--- vanilla-ts/src/main.ts
+++ vanilla-ts/src/main.ts
@@ -1,8 +1,23 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+
+export const $ = (selectors: string) => {
+  return document.querySelector(selectors);
+};
+export const $$ = (selectors: string) => {
+  return document.querySelectorAll(selectors);
+};
+const app = $('#app')!
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>计算 setTimeout 和 setInterval 的误差</h1>
+  <button id="setIntervalDelay">
+    setIntervalDelay
+  </button>
 `
+function setIntervalDelay() {
+  setInterval(() => {
+    app.innerHTML += `<li></li>`
+  }, 1000)
+}
+$('#setIntervalDelay')!.addEventListener('click', setIntervalDelay)
