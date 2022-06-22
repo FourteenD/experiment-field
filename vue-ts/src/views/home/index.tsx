c473094b82fe8a503a8477d9f9ae1d4c7f873925
FourteenD <2899385645@qq.com>
2022-06-22 22:27:55 +0800

    up

--- vue-ts/src/views/home/index.tsx
+++ vue-ts/src/views/home/index.tsx
@@ -1,27 +1,10 @@
 import { defineComponent } from "vue";
 
-import style from "./index.module.styl";
-
 export default defineComponent({
-  name: "Home",
+  name: "App",
   setup() {
-    type todoItem = {
-      name: string;
-      state: "onfinish" | "finish";
-    };
-    const todoList = $ref<Array<todoItem>>([]);
-    const addItem = () => {
-      todoList.push({
-        name: "我是代办事项",
-        state: "onfinish",
-      });
-    };
-    return () => {
-      return (
-        <>
-          <div class={style.title}>Home</div>
-        </>
-      );
-    };
+    return <>
+      
+    </>;
   },
 });
