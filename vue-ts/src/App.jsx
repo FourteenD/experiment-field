5787e64d1052b2ad59b0887b1e6010c23f75c0d8
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-22 05:48:06 +0000

    up

--- vue-ts/src/App.jsx
+++ vue-ts/src/App.jsx
@@ -1,11 +1,8 @@
 import { defineComponent } from 'vue'
+
 export default defineComponent({
   name: 'App',
-  setup() {
-
-    return <><div>test2</div></>
-  },
   render() {
-    return <div>Test1</div>
+    return <div>{this.msg}</div>
   }
 })
