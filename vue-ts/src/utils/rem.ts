78fb6c9218375ecb4750b2008ac4234074bff453
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-22 09:22:55 +0000

    up

--- vue-ts/src/utils/rem.ts
+++ vue-ts/src/utils/rem.ts
@@ -1,6 +1,6 @@
 export const setRemSize = (logicWidth = 750) => {
   const screenWidth = document.documentElement.clientWidth
-  const rpx = screenWidth / logicWidth
+  const rpx = screenWidth * 1000 / logicWidth
   document.documentElement.style.fontSize = rpx + 'px'
   window.onresize = function () {
     setRemSize()
