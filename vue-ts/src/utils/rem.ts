45dd6a2e8f0127a932d01aab8f36d65da5b7653f
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-22 10:00:41 +0000

    up

--- vue-ts/src/utils/rem.ts
+++ vue-ts/src/utils/rem.ts
@@ -1,11 +1,11 @@
 // rem移动端适配
-export const setRemSize = (logicWidth = 750) => {
+export const setRemSize = (logicWidth = 750, remPx = 100) => {
   // 页面可视宽度
   const screenWidth = document.documentElement.clientWidth
   // 1rem像素
-  const remPx = screenWidth * 100 / logicWidth
+  const rpx = screenWidth * remPx / logicWidth
 
-  document.documentElement.style.fontSize = remPx + 'px'
+  document.documentElement.style.fontSize = rpx + 'px'
   window.onresize = function () {
     setRemSize()
   }
