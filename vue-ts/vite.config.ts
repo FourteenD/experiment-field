5dd658033e8f6b8db682e79cda44453420aef722
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-23 09:00:45 +0000

    up

--- vue-ts/vite.config.ts
+++ vue-ts/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { resolve } from "path"
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,14 +13,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": resolve(__dirname, "/src"),
+      vue: "vue/dist/vue.esm-bundler.js"
     }
   },
   css: {
     preprocessorOptions: {
       styl: {
-        devSourcemap: true,
-        additionalData: `@import "./src/common/styl/base.styl";`
+        imports: [resolve(__dirname, "./src/common/styl/base.styl")]
       }
     }
   },
