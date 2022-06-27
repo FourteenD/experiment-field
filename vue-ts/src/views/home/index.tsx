0fda7a0228ace882fafba5f68c286256389fb0b4
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-27 13:16:40 +0000

    up

--- vue-ts/src/views/home/index.tsx
+++ vue-ts/src/views/home/index.tsx
@@ -1,6 +1,6 @@
 import { defineComponent, onMounted } from "vue";
 
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 
 import NavBar from "@/components/NavBar";
 import HomeMenuItem from "./components/MenuItem";
@@ -8,23 +8,27 @@ import HomeMenuItem from "./components/MenuItem";
 export default defineComponent({
   name: "Home",
   setup() {
-    const router = useRouter()
+    const router = useRouter();
     const menuList = $ref([
       {
-        emoji: '📅',
+        emoji: "📅",
         title: "TodoList",
         path: "/todoList",
       },
-    ])
-    const item = menuList.map((item, index) =>
-      <HomeMenuItem onClick={() => clickHandler({ item, index })} emoji={item.emoji} title={item.title}></HomeMenuItem>
-    )
+    ]);
+    const item = menuList.map((item, index) => (
+      <HomeMenuItem
+        onClick={() => clickHandler({ item, index })}
+        emoji={item.emoji}
+        title={item.title}
+      ></HomeMenuItem>
+    ));
     const clickHandler: (...args: any[]) => void = ({ item }) => {
-      router.push(`@/views${item.path}`)
-    }
+      router.push(`@/views${item.path}`);
+    };
     return () => (
       <>
-        <NavBar title='Home'></NavBar>
+        <NavBar title="Home"></NavBar>
         {item}
       </>
     );
