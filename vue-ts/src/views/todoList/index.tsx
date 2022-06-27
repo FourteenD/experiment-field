6ceb15a47f4b862a73a7ba8f7d59d785f90c39c5
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-27 13:15:20 +0000

    up

--- vue-ts/src/views/todoList/index.tsx
+++ vue-ts/src/views/todoList/index.tsx
@@ -5,33 +5,29 @@ import AddInput from "./components/Bottom";
 import NavBar from "@/components/NavBar";
 import TodoListItem from "./components/TodoListItem";
 
-
 export default defineComponent({
   name: "TodoList",
   setup() {
-    const unfinished = $ref<Type.TodoList>([])
-    const finished = $ref<Type.TodoList>([])
-    const todoList = $computed(() => [...unfinished, ...finished])
-
+    const unfinished = $ref<Type.TodoList>([]);
+    const finished = $ref<Type.TodoList>([]);
+    const todoList = $computed(() => [...unfinished, ...finished]);
     const addTodoItem = (title: string) => {
       unfinished.push({
         title,
         state: "unfinished",
-      })
-    }
+      });
+    };
     const removeTodoItem = (index: number) => {
-      unfinished.splice(index, 1)
-    }
+      unfinished.splice(index, 1);
+    };
     return () => (
       <>
         <NavBar title="Todo List"></NavBar>
-        {
-          todoList.map((item) =>
-            <TodoListItem></TodoListItem>
-          )
-        }
+        {todoList.map((item) => (
+          <TodoListItem></TodoListItem>
+        ))}
         <AddInput onConfirm={(e) => addTodoItem(e)}></AddInput>
       </>
-    )
+    );
   },
 });
