a65278d0c9d711f285b5918aa64ab2b16b0a8344
Fourteen <53899166+FourteenD@users.noreply.github.com>
2022-06-28 10:00:09 +0000

    up

--- vue-ts/src/views/todoList/index.tsx
+++ vue-ts/src/views/todoList/index.tsx
@@ -1,13 +1,17 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref, watch } from "vue";
 import * as Type from "./type";
 
 import Bottom from "./components/Bottom";
 import NavBar from "@/components/NavBar";
 import TodoListItem from "./components/TodoListItem";
+import Input from "./components/Input";
 
+import styl from "./index.module.styl";
+import { TodoItem } from "./type";
 export default defineComponent({
   name: "TodoList",
   setup() {
+    let content = $ref<any>()
     const todoList = $ref<Type.TodoList>([]);
 
     const addTodoItem = (title: string) => {
@@ -15,9 +19,11 @@ export default defineComponent({
         title,
         state: "unfinished",
       });
+      setTodoList(todoList)
     };
     const removeTodoItem = (index: number) => {
       todoList.splice(index, 1);
+      setTodoList(todoList)
     };
 
     const toggleTodoItem = (index: number) => {
@@ -25,12 +31,13 @@ export default defineComponent({
         todoList[index].state === "finished" ? "unfinished" : "finished";
       todoList.push(todoList[index]);
       todoList.splice(index, 1);
+      setTodoList(todoList)
     };
 
-    return () => (
-      <>
-        <NavBar title="Todo List"></NavBar>
-        {todoList.map((item, index) => (
+
+    const setTodoList = (e: Type.TodoList) => {
+      content = e.map(
+        (item, index) => (
           <TodoListItem
             key={index}
             title={item.title}
@@ -38,7 +45,26 @@ export default defineComponent({
             onDel={() => removeTodoItem(index)}
             onCheck={() => toggleTodoItem(index)}
           ></TodoListItem>
-        ))}
+        )
+      )
+    }
+    const searchContent = $ref("");
+    watch(() => searchContent, (nVal, oVal) => {
+      if (nVal.length) {
+        todoList.filter((item) => {
+          return
+        })
+      }
+    }, { deep: true, immediate: true });
+
+
+    return () => (
+      <>
+        <NavBar title="Todo List"></NavBar>
+        <div class={styl.search}>
+          <Input v-model={searchContent} placeholder="Search..."></Input>
+        </div>
+        {content}
         <Bottom onConfirm={(e) => addTodoItem(e)}></Bottom>
       </>
     );
