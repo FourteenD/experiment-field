0d5dc7bc2d167ceeb873f22740c7f52da6e82f0a
FourteenD <2899385645@qq.com>
2022-06-28 22:47:56 +0800

    up

--- vue-ts/src/views/todoList/index.tsx
+++ vue-ts/src/views/todoList/index.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref, watch } from "vue";
+import { defineComponent, watch } from "vue";
 import * as Type from "./type";
 
 import Bottom from "./components/Bottom";
@@ -7,11 +7,10 @@ import TodoListItem from "./components/TodoListItem";
 import Input from "./components/Input";
 
 import styl from "./index.module.styl";
-import { TodoItem } from "./type";
 export default defineComponent({
   name: "TodoList",
   setup() {
-    let content = $ref<any>()
+    let content = $ref<any>();
     const todoList = $ref<Type.TodoList>([]);
 
     const addTodoItem = (title: string) => {
@@ -19,11 +18,11 @@ export default defineComponent({
         title,
         state: "unfinished",
       });
-      setTodoList(todoList)
+      setTodoList(todoList);
     };
     const removeTodoItem = (index: number) => {
       todoList.splice(index, 1);
-      setTodoList(todoList)
+      setTodoList(todoList);
     };
 
     const toggleTodoItem = (index: number) => {
@@ -31,32 +30,32 @@ export default defineComponent({
         todoList[index].state === "finished" ? "unfinished" : "finished";
       todoList.push(todoList[index]);
       todoList.splice(index, 1);
-      setTodoList(todoList)
+      setTodoList(todoList);
     };
 
-
     const setTodoList = (e: Type.TodoList) => {
-      content = e.map(
-        (item, index) => (
-          <TodoListItem
-            key={index}
-            title={item.title}
-            isActive={item.state == "finished"}
-            onDel={() => removeTodoItem(index)}
-            onCheck={() => toggleTodoItem(index)}
-          ></TodoListItem>
-        )
-      )
-    }
+      content = e.map((item, index) => (
+        <TodoListItem
+          key={index}
+          title={item.title}
+          isActive={item.state == "finished"}
+          onDel={() => removeTodoItem(index)}
+          onCheck={() => toggleTodoItem(index)}
+        ></TodoListItem>
+      ));
+    };
     const searchContent = $ref("");
-    watch(() => searchContent, (nVal, oVal) => {
-      if (nVal.length) {
-        todoList.filter((item) => {
-          return
-        })
-      }
-    }, { deep: true, immediate: true });
-
+    watch(
+      () => searchContent,
+      (nVal, oVal) => {
+        if (nVal.length) {
+          todoList.filter((item) => {
+            return;
+          });
+        }
+      },
+      { deep: true, immediate: true }
+    );
 
     return () => (
       <>
